e0a07844dc28df818e0b8738cff6376e9112623a
Weefle <mr.weefle@gmail.com>
2020-10-07 09:16:14 +0200

    Ajout objet Animal

--- main.c
+++ main.c
@@ -1,5 +1,35 @@
+#include <stdbool.h>
+#include <stdio.h>
 #include "verifications.h"
-int main() {
 
-    return 0;
+
+typedef struct Animal Animal;
+
+struct Animal{
+
+    char type;
+    int x;
+    int y;
+
+};
+
+void main() {
+
+    Animal animal;
+
+    animal.type = 'E';
+    animal.x = 7;
+    animal.y = 9;
+
+
+    while (true) {
+
+        printf("Bonjour à toi joueur intrépide, dit moi ton nom");
+
+
+
+        printf("Type: %c, %i, %i\n", animal.type, animal.x, animal.y);
+        return;
+
+    }
 }
--- verifications.c
+++ verifications.c
@@ -6,3 +6,5 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
+
