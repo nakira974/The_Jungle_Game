1eb34e649f7d0b261d90ae1cd0e664de9f3db8b3
nakira974 <maxime.loukhal@gmail.com>
2020-11-19 23:35:34 +0100

    Refonte complète du multijoueur pour windows uniquement, serveur et client OK, communiction localhost OK

---
+++ multiplayer/client1.h
@@ -0,0 +1,10 @@
+//
+// Created by max on 19/11/2020.
+//
+
+#ifndef PROJETJUNGLE_CLIENT1_H
+#define PROJETJUNGLE_CLIENT1_H
+
+int __cdecl app_client1(char *srvAdd);
+
+#endif //PROJETJUNGLE_CLIENT1_H
---
+++ multiplayer/server1.h
@@ -0,0 +1,10 @@
+//
+// Created by max on 19/11/2020.
+//
+
+#ifndef PROJETJUNGLE_SERVER1_H
+#define PROJETJUNGLE_SERVER1_H
+
+int __cdecl app_serv1(void);
+
+#endif //PROJETJUNGLE_SERVER1_H
